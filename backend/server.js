e294997fa9fd4ecd0d0cba053b1f5a05e5e1da92
James McNeil <mcneil_james@hotmail.com>
2022-04-27 07:43:58 +0100

    created errorHandler middleware import to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express') // import express package
 const dotenv = require('dotenv').config() // allows to read data from .env file
+const { errorHandler } = require('./middleware/errorMiddleware') // import customer error handling middleware
 
 const PORT = process.env.PORT || 5000 // specify port for the backend to list on pulled from .env
 
 const app = express() // initiate express application on app
 
 app.use('/api/todos', require('./routes/todoRoutes')) // direct /api/todos to todoRoutes router
+app.use(errorHandler) // set express application to use custom error handling middleware
 
 app.listen(PORT, () => {
   console.log(`server started om port: ${PORT}`) // set app to listen on specified port
